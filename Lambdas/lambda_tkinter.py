7c3fcd25adcf4504808b761bd2aa2009f0ded519
korntech <65980885+korntech@users.noreply.github.com>
2020-06-03 14:27:48 +0200

    Update lambda_tkinter.py

--- Lambdas/lambda_tkinter.py
+++ Lambdas/lambda_tkinter.py
@@ -8,7 +8,7 @@ master.title("Learning lambda...")
 master.geometry("400x300")
 master.grid_columnconfigure(0, weight=1)
 
-button = tk.Button(master, text="Click me", command=lambda: print_sth("Learning lambda..")
+button = tk.Button(master, text="Click me", command=lambda: print_sth("Learning lambda.."))
 button.grid(column=0, row=1)
 
 master.mainloop()
