85a56ef0a334408da2695f3dce88177f0e34200d
korntech <65980885+korntech@users.noreply.github.com>
2020-06-03 14:26:22 +0200

    Create lambda_tkinter.py

---
+++ Lambdas/lambda_tkinter.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+
+def print_sth(text):
+    print(text)
+
+master = tk.Tk()
+master.title("Learning lambda...")
+master.geometry("400x300")
+master.grid_columnconfigure(0, weight=1)
+
+button = tk.Button(master, text="Click me", command=lambda: print_sth("Learning lambda..")
+button.grid(column=0, row=1)
+
+master.mainloop()
