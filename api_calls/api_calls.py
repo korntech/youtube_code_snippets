02cac344e5a006fca08cd58a22dc61fe1030b424
korntech <65980885+korntech@users.noreply.github.com>
2020-06-09 18:32:39 +0200

    Update api_calls.py

--- api_calls/api_calls.py
+++ api_calls/api_calls.py
@@ -1,1 +1,19 @@
+# https://restcountries.eu/#api-endpoints-all
 
+import requests
+
+r = requqests.get('https://restcountries.eu/#api-endpoints-all')
+
+#Use json decoder to decode the response
+resp = r.json()
+
+for item in resp:
+  print(f"Country: {item['name']})
+  print(f"Country: {item['region']})
+  print(f"Country: {item['capital']})
+  print(20 * '-')
+  
+  #Listcomp used to filter out specific records 
+  [item for item in resp if item['name'] == 'Poland'] 
+  
+  
